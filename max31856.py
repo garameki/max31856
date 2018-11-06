3b29514e57ebd5ef6d5882e45f78c32d13874b1f
garameki <usakutakahashi97@gmail.com>
2018-11-06 17:02:31 +0900

    change code to module

--- max31856.py
+++ max31856.py
@@ -1,72 +1,109 @@
+#!/usr/bin/env python3
 # _*_ coding:utf-8 _*_
 
 import sys
-import codecs
-from time import sleep
+import time
 import spidev
 
 
-#関数
-filename = "max31856.txt"	#権限は -rw-rw---- www-data www-data
-def fwrite(valueFlt,valueHJ,valueCJ):
-	global filename
-	file = codecs.open(filename,"w","utf-8")
-	jsonResult = "{{FAULT:{0}\nHJ:{1}\nCJ:{2}\n}}".format(valueFlt,valueHJ,valueCJ)
-	file.write(jsonResult)
-	file.close()
-	print(5)
+class Max31856:
 
-dummy = 0
+	spi = None
+	DUMMY = 0
 
-spi = spidev.SpiDev()
-spi.open(0,0)			#/sys/bus/spi/devices/dev0.0を使う
-spi.mode = 0x03			#モード3 CPOL:1 CPHA:1 ,Especially CPHA must be 1
-spi.max_speed_hz = 1000000	#最大クロック周波数
+	def __init__(self):
+		#ここで
+		#dev0.0 dev0.1 dev1.0の選択
+		#B,T,R,K,...の選択
+		#verboseの選択
+		#などをするべきだろう
+		pass
 
-args = sys.argv
-if len(args)==2:
-	if args[1] == "reset":
+
+	def open(self):
+		if self.spi is None:
+			self.spi = spidev.SpiDev()
+			self.spi.open(0,0)			#/sys/bus/spi/devices/dev0.0を使う
+			self.spi.mode = 0x03			#モード3 CPOL:1 CPHA:1 ,Especially CPHA must be 1
+			self.spi.max_speed_hz = 1000000	#最大クロック周波数
+
+	def close(self):
+		if self.spi is not None:
+			self.spi.close()
+			self.spi = None
+
+	def reset(self):
 		#FAULTピンのリセット
-		resp = spi.xfer2([0x80,0x17])
-		sleep(1)
+		resp = self.spi.xfer2([0x80,0x17])
 
-try:
-	while True:
+	def read(self):
+		dummy = self.DUMMY
+
 		#通常計測
 		#xfer2はcsを下げたまま
 		#xferは1バイトごとにcsを下げ上げする
-		resp = spi.xfer2([0x80,0x55,0x35,0x00,0x3C,0xF6,0x57,0x80,0xFE,0xC0,0x00])	#設定と同時に計測するのでCR0の第6ビットを立てる
-		sleep(1)			#計測を待つ
+		resp = self.spi.xfer2([0x80,0x55,0x35,0x00,0x3C,0xF6,0x57,0x80,0xFE,0xC0,0x00])	#設定と同時に計測するのでCR0の第6ビットを立てる
+		time.sleep(1)			#計測を待つ
 
 
 		#Faultの検知
-		resp = spi.xfer([0x0f,dummy])
+		resp = self.spi.xfer([0x0f,dummy])
 		if (resp[1] & 0xFF) != 0:
 			valueFlt = resp[1]
 			valueHJ = ""
 			valueCJ = ""
-			fwrite(valueFlt,valueHJ,valueCJ)
-			print(resp)
-			break
 		else:
 
 			valueFlt = resp[1]
 
 			#熱電対の温度を読み込み
-			resp = spi.xfer2([0x0C,0x0D,0x0E,dummy])
+			resp = self.spi.xfer2([0x0C,0x0D,0x0E,dummy])
 			valueHJ = resp[1] * 256 + resp[2]
 			if (resp[1] & 0x80) != 0:
 				valueHJ = -1 * (~(valueHJ - 1) & 0x7FFF)	#2の補数の10進数化
 
 			#冷接点の温度を読み込み
-			resp = spi.xfer2([0x0A,0x0B,dummy])
+			resp = self.spi.xfer2([0x0A,0x0B,dummy])
 			valueCJ = (resp[1] << 6)  + (resp[2] >> 2)
 			if (resp[1] & 0x80) != 0:
 				valueCJ = -1 * (~(valueCJ - 1) & 0x7FFF)	#2の補数の10進数化
 
-			fwrite(valueFlt,valueHJ,valueCJ)
-			print(6)
-except KeyboardInterrupt:
-	pass
-finally:
-	spi.close()
+		return self._format(valueFlt,valueHJ,valueCJ)
+
+	def _format(self,valueFlt,valueHJ,valueCJ):
+		jsonFormat = "{{FAULT:{0}\nHJ:{1}\nCJ:{2}\n}}".format(valueFlt,valueHJ,valueCJ)
+		return jsonFormat
+
+	def write_file(filepath,jsonFormat):
+		file = codecs.open(filepath,"w","utf-8")
+		file.write(jsonFormat)
+		file.close()
+
+if __name__ == '__main__':
+
+	filepath = '/home/pi/www/garameki/raspberrypi/cgi-bin/max31856.txt'
+
+	spi0 = Max31856()
+	if "reset" in sys.argv:
+		spi0.open()
+		spi0.reset()
+		spi0.close()
+	else:
+		spi0.open()
+		print(spi0.read())
+		spi0.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
